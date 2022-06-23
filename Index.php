e84bcb32b3a441e01a5736e502ff5a7b82df6921
Yufan(Miranda) Wu <58931129+ywu279@users.noreply.github.com>
2022-06-23 14:28:09 -0400

    Update Index.php

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@
     <h1>Welcome to Algonquin Bank</h1>
     <p>Algonquin Bank is Algonquin College students' most loved bank. We provide a set of tools for Algonquin College students to manage their finance</p>
     <ul>
-        <li><a href="DepositCalculator.php">Deposit Calculator</a></li>
+        <li><a href="Disclaimer.php">Deposit Calculator</a></li>
     </ul>
 </div>
 
